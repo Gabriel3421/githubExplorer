be1e971804b46f71f1494c87edf55050f966ff01
gabriel3421 <gabrielsouza.n.s@hotmail.com>
2020-11-11 20:36:42 -0300

    lidando com erros do input

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,6 @@
 import React, { FormEvent, useState } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
-import { Title, Form, Repositories } from './style';
+import { Title, Form, Repositories, Error } from './style';
 import logo from '../../assets/logo.svg';
 import api from '../../services/api';
 
@@ -15,22 +15,32 @@ interface Repository {
 
 const Dashboard: React.FC = () => {
   const [nr, setNr] = useState('');
+  const [inputErr, setInputErr] = useState('');
   const [repos, setRepos] = useState<Repository[]>([]);
 
   async function handleAddRepository(
     event: FormEvent<HTMLFormElement>,
   ): Promise<void> {
     event.preventDefault();
-    const response = await api.get(`repo/${nr}`);
-    setRepos([...repos, response.data]);
-    setNr('');
+    if (!nr) {
+      setInputErr('Digite o autor/nome do repo');
+      return;
+    }
+    try {
+      const response = await api.get(`repos/${nr}`);
+      setRepos([...repos, response.data]);
+      setNr('');
+      setInputErr('');
+    } catch (error) {
+      setInputErr('Erro ao buscar repositorio, verifique os dados');
+    }
   }
 
   return (
     <>
       <img src={logo} alt="" />
       <Title>Explore repositórios no Github.</Title>
-      <Form onSubmit={handleAddRepository}>
+      <Form hasError={!!inputErr} onSubmit={handleAddRepository}>
         <input
           placeholder="Digite aqui"
           value={nr}
@@ -38,6 +48,7 @@ const Dashboard: React.FC = () => {
         />
         <button type="submit">Pesquisar</button>
       </Form>
+      {inputErr && <Error>{inputErr}</Error>}
       <Repositories>
         {repos.map(repo => (
           <a key={repo.full_name} href="piroca">
