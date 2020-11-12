0e75e47d19892cdd5fb1f658b171b1b053a69cfc
gabriel3421 <gabrielsouza.n.s@hotmail.com>
2020-11-12 20:12:24 -0300

    estilizando detalhes do repositorio

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { Header, RepositoryInfo, Issues, Error } from './style';
+import logo from '../../assets/logo.svg';
 
 interface RepositoryParams {
   repo: string;
@@ -7,7 +10,41 @@ interface RepositoryParams {
 
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
-  return <h1>Repository:{params.repo}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logo} alt="" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+      <RepositoryInfo>
+        <header>
+          <img src="" alt="" />
+          <div>
+            <strong>asdas</strong>
+            <p>asda</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>123</strong>
+            <span>sdasd</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+      <Issues>
+        <Link to="/">
+          <div>
+            <strong>full_name</strong>
+            <p>description</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
